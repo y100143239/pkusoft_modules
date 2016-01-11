524da73c66f2e8b802a26f9629d3bc80518ee40d
wuqinfei <wuqinfei_cs@163.com>
2016-01-11 23:14:31 +0800

    内蒙-平台，完成地图页数据更新

--- dev/apps/workbench/js/main.js
+++ dev/apps/workbench/js/main.js
@@ -1,11 +1,13 @@
 +function ( $ ) {
     var qjsj = {}; // 区级数据
     qjsj.map = {
+        $container: null,
         $items: null,
         init : function () {
             this.render().bind();
         },
         render: function () {
+            this.$container = $( ".js--qjsj-data" );
             this.$items = $( ".map-hot-item" );
             return this;
         },
@@ -29,6 +31,18 @@
         },
         _updateData: function _updateData( cityId ) {
             // 发送 Ajax 请求
+            // 处理数据
+            var template = Template.qjsj.template;
+            var data = Template.qjsj.data;
+            var html;
+
+            data[0 ].heading.cont = "3333_" + cityId;
+            data[1 ].heading.cont = "3333_" + cityId;
+            data[2 ].heading.cont = "3333_" + cityId;
+
+            html = doT.template( template )( data );
+
+            this.$container.html( html );
 
             return this;
         }
@@ -38,5 +52,42 @@
         // 区级数据
         qjsj.map.init();
     } );
+
+
+    var Template = {
+        qjsj: {
+            data: [
+                { heading: { title: "人口", icon: "data_icon_1", label: "总数：", cont: "333333_" },
+                    body: [ { label: "女性：", "cont": "22222_"}, { label: "男性：", "cont": "11111_"} ]
+                },
+                { heading: { title: "户", icon: "data_icon_2", label: "总数：", cont: "4444_" },
+                    body: [ { label: "家庭户：", "cont": "3333_"}, { label: "集体户：", "cont": "0001_"}, { label: "农业户：", "cont": "0010_" }, { label: "城镇户：", "cont": "1100_" } ]
+                },
+                { heading: { title: "地址", icon: "data_icon_3", label: "总数：", cont: "99999_" },
+                    body: []
+                }
+            ],
+            template:
+                '{{~it:value:index}}\
+                    <div class="data-panel">\
+                        <div class="panel-heading">\
+                            <p class="total"><span class="label">{{= value.heading.label }}</span><span class="cont">{{= value.heading.cont }}</span></p>\
+                            <img class="icon" src="./images/{{= value.heading.icon }}.png"><span class="title">{{= value.heading.title }} </span>\
+                        </div>\
+                        {{? value.body && value.body.length > 0  }}\
+                            <div class="panel-body">\
+                            {{~value.body:value2:index2}}\
+                                <div class="data-item {{? index2 % 2 === 0 }}line-first{{?}}">\
+                                    <span class="label">{{= value2.label }}</span><span class="cont">{{= value2.cont }}</span>\
+                                </div>\
+                            {{~}}\
+                            </div>\
+                        {{?}}\
+                    </div>\
+                {{~}}'
+
+}
+
+    };
 
 }( jQuery );
