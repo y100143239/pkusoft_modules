c74c51c6f2ac433288675b6c5f0540fea2ad4230
wuqinfei <wuqinfei_cs@163.com>
2016-03-01 17:13:29 +0800

    完成内蒙工作台“统计分析”

---
+++ dev/modules/jquery/plugin/datepicker/datepicker_lang_CN.js
@@ -0,0 +1,37 @@
+﻿var i18n = $.extend({}, i18n || {}, {
+    datepicker: {
+        dateformat: {
+            "fulldayvalue": "yyyy-MM-dd",
+            "separator": "-",
+            "year_index": 0,
+            "month_index": 1,
+            "day_index": 2,
+            "sun": "日",
+            "mon": "一",
+            "tue": "二",
+            "wed": "三",
+            "thu": "四",
+            "fri": "五",
+            "sat": "六",
+            "jan": "1月",
+            "feb": "2月",
+            "mar": "3月",
+            "apr": "4月",
+            "may": "5月",
+            "jun": "6月",
+            "jul": "7月",
+            "aug": "8月",
+            "sep": "9月",
+            "oct": "10月",
+            "nov": "11月",
+            "dec": "12月",
+            "postfix": ""
+        },
+        ok: "确定",
+        cancel: "取消",
+        today: "今天",
+        prev_month_title: "上一月",
+        next_month_title: "下一月"
+    }
+});
+
