9596edde20d8d387ec1b5c80f8f47465aa7f963a
wuqinfei <wuqinfei_cs@163.com>
2016-07-22 16:53:50 +0800

    * 项目构建，及完成formvalidation的文档

---
+++ dist/apps/framework/js/main.js
@@ -0,0 +1,51 @@
+define( [ "jquery", "bootstrap", "formvalidationI18N" ], function ( $ ) {
+
+    $( document ).ready( function () {
+        var $main,
+            $body
+            ;
+        $main = $( "main.main" );
+        $body = $( document.body );
+
+        $( ".sidebar-menu" ).on( "click.title", ".treeview > a", function () {
+            var $this,
+                $arrow,
+                $menu
+                ;
+            $this = $( this );
+            $arrow = $this.find( ".fa-angle-left" );
+            $menu = $this.next();
+
+            $arrow.toggleClass( "fa-angle-down" );
+            //
+            if ( $arrow.is( ".fa-angle-down" ) ) {
+                $menu.show();
+            } else {
+                $menu.hide();
+            }
+            return false;
+        } ).on( "click.page", ".treeview-menu a", function () {
+            var $this;
+            $this = $( this );
+            if ( $this.is( "._outerlink" ) ) {
+                return;
+            }
+            $body.append( '<div class="overlay overlay-black"><i class="fa fa-spinner fa-spin"></i></div>' );
+
+            $main.load( $( this ).attr( "href" ), function successCallback() {
+                //alert( "loaded" );
+                $body.children( ".overlay" ).remove();
+                window["PR"] && window["PR"].prettyPrint();
+            } );
+            $this.closest( ".sidebar-menu" ).find( ".active" ).removeClass( "active" );
+            $this.parent().addClass( "active" );
+            $this.closest( ".treeview" ).addClass( "active" );
+            window["PR"] && window["PR"].prettyPrint();
+            return false;
+        } );
+
+        //$( ".treeview-menu .active a" ).trigger( "click" );
+
+    } );
+
+} );
